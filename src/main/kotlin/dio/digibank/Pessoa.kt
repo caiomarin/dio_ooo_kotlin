3aa630ce836215448fd6ee98863d532d84f4697f
caiomarin <caiomarin@gmail.com>
2021-07-12 11:08:00 -0300

    Update Pessoa.kt

--- src/main/kotlin/dio/digibank/Pessoa.kt
+++ src/main/kotlin/dio/digibank/Pessoa.kt
@@ -3,9 +3,14 @@ package dio.digibank
 class Pessoa {
     var nome: String = "Caio"
     var cpf: String = "363.930.068-89"
+    private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
     val jether = Pessoa()
-    println(jether.nome)
+    println(jether.pessoaInfo())
 }
